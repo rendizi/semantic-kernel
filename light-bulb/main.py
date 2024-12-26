81f854d0cf829e242e89008cfce6c303af644648
rendizi <baglanov.a0930@gmail.com>
2024-12-26 13:45:28 +0500

    light bulb ex

---
+++ light-bulb/main.py
@@ -0,0 +1,88 @@
+import asyncio
+
+from semantic_kernel import Kernel
+from semantic_kernel.utils.logging import setup_logging
+from semantic_kernel.functions import kernel_function
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
+from semantic_kernel.contents.chat_history import ChatHistory
+from semantic_kernel.functions.kernel_arguments import KernelArguments
+
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import (
+    AzureChatPromptExecutionSettings,
+)
+from dotenv import load_dotenv
+import os
+import logging
+from plugin import LightsPlugin
+
+load_dotenv()
+
+deployment_name = os.getenv("AZURE_DEPLOYMENT_NAME")
+api_key = os.getenv("AZURE_API_KEY")
+base_url = os.getenv("AZURE_BASE_URL")
+
+print(base_url)
+
+async def main():
+    # Initialize the kernel
+    kernel = Kernel()
+
+    # Add Azure OpenAI chat completion
+    chat_completion = AzureChatCompletion(
+        deployment_name=deployment_name,
+        api_key=api_key,
+        base_url=base_url,
+    )
+    kernel.add_service(chat_completion)
+
+    # Set the logging level for  semantic_kernel.kernel to DEBUG.
+    logging.basicConfig(
+        format="[%(asctime)s - %(name)s:%(lineno)d - %(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logging.getLogger("kernel").setLevel(logging.DEBUG)
+
+    # Add a plugin (the LightsPlugin class is defined below)
+    kernel.add_plugin(
+        LightsPlugin(),
+        plugin_name="Lights",
+    )
+
+    # Enable planning
+    execution_settings = AzureChatPromptExecutionSettings()
+
+
+    # Create a history of the conversation
+    history = ChatHistory()
+
+    # Initiate a back-and-forth chat
+    userInput = None
+    while True:
+        # Collect user input
+        userInput = input("User > ")
+
+        # Terminate the loop if the user says "exit"
+        if userInput == "exit":
+            break
+
+        # Add user input to the history
+        history.add_user_message(userInput)
+
+        # Get the response from the AI
+        result = await chat_completion.get_chat_message_content(
+            chat_history=history,
+            settings=execution_settings,
+            kernel=kernel,
+        )
+
+        # Print the results
+        print("Assistant > " + str(result))
+
+        # Add the message from the agent to the chat history
+        history.add_message(result)
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.run(main())
